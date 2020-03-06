ddc17d26413222338224585320abeead39e2a143
Sevgi AKTEN <sevgiakten@gmail.com>
2020-03-06 11:25:04 +0300

    Data classes ara created and entity frameweork core and other some important packages are installed

---
+++ ProjectSupportSystem/Data/Base.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSupportSystem.Data
+{
+	public class Base
+	{
+		public Guid ID { get; set; }
+		public DateTime ModifiedDate { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public string Description { get; set; } 
+	}
+}
---
+++ ProjectSupportSystem/Data/SupportElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSupportSystem.Data
+{
+	public class SupportElement : Base
+	{
+		public string SupportElementName { get; set; }
+		public int SupportElementPercentage { get; set; }
+		public decimal SupportElementAmount { get; set; }
+		public Guid SupPropSupElemID { get; set; }
+		public virtual List<SupPropSupElem> SupPropElemsOfSupportElement { get; set; } 
+	}
+}
