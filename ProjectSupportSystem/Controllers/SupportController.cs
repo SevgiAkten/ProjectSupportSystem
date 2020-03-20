1f6279732fe46899919119a9e6a688c84235feb1
Sevgi AKTEN <sevgiakten@gmail.com>
2020-03-20 12:08:11 +0300

    AddOrUpdate action is written, some icons add ui elements editted

--- ProjectSupportSystem/Controllers/SupportController.cs
+++ ProjectSupportSystem/Controllers/SupportController.cs
@@ -24,26 +24,9 @@ namespace ProjectSupportSystem.Controllers
 			return View(await _context.Supports.ToListAsync());
 		}
 
-		// GET: Support/Details/5
-		public async Task<IActionResult> Details(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
-
-			var support = await _context.Supports
-				.FirstOrDefaultAsync(m => m.ID == id);
-			if (support == null)
-			{
-				return NotFound();
-			}
-
-			return View(support);
-		}
 
 		// GET: Support/Create
-		public IActionResult Create()
+		public IActionResult AddOrUpdate(int id = 0)
 		{
 			var SupportElements = _context.SupportElements.ToListAsync();
 
@@ -56,7 +39,16 @@ namespace ProjectSupportSystem.Controllers
 				ViewBag.SupportElements = "Boş ya da null geldi!";
 			}
 
-			return View();
+			var support = new Support();
+			if (id == 0)
+			{
+				return View(support);
+			}
+			else
+			{
+				support = _context.Supports.Find(id);
+				return View(support);
+			}
 		}
 
 		// POST: Support/Create
@@ -64,63 +56,15 @@ namespace ProjectSupportSystem.Controllers
 		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Create([Bind("SupportType,SupportName,WhoApply,Goal,ApplicationProcessAndCondition,SupportDuration,SupportPercentage,SupportAmount,Code,SupportSupElementID,ID,Description")] Support support)
+		public async Task<IActionResult> AddOrUpdate([Bind("SupportType,SupportName,WhoApply,Goal,ApplicationProcessAndCondition,SupportDuration,SupportPercentage,SupportAmount,Code,SupportSupElementID,ID,Description")] Support support)
 		{
 			if (ModelState.IsValid)
 			{
-				_context.Add(support);
-				await _context.SaveChangesAsync();
-				return RedirectToAction(nameof(Index));
-			}
-			return View(support);
-		}
-
-		// GET: Support/Edit/5
-		public async Task<IActionResult> Edit(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
-
-			var support = await _context.Supports.FindAsync(id);
-			if (support == null)
-			{
-				return NotFound();
-			}
-			return View(support);
-		}
-
-		// POST: Support/Edit/5
-		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(int id, [Bind("SupportType,SupportName,WhoApply,Goal,ApplicationProcessAndCondition,SupportDuration,SupportPercentage,SupportAmount,Code,SupportSupElementID,ID,Description")] Support support)
-		{
-			if (id != support.ID)
-			{
-				return NotFound();
-			}
-
-			if (ModelState.IsValid)
-			{
-				try
-				{
+				if (support.ID == 0)
+					_context.Add(support);
+				else
 					_context.Update(support);
-					await _context.SaveChangesAsync();
-				}
-				catch (DbUpdateConcurrencyException)
-				{
-					if (!SupportExists(support.ID))
-					{
-						return NotFound();
-					}
-					else
-					{
-						throw;
-					}
-				}
+				await _context.SaveChangesAsync();
 				return RedirectToAction(nameof(Index));
 			}
 			return View(support);
@@ -129,35 +73,12 @@ namespace ProjectSupportSystem.Controllers
 		// GET: Support/Delete/5
 		public async Task<IActionResult> Delete(int? id)
 		{
-			if (id == null)
-			{
-				return NotFound();
-			}
-
-			var support = await _context.Supports
-				.FirstOrDefaultAsync(m => m.ID == id);
-			if (support == null)
-			{
-				return NotFound();
-			}
-
-			return View(support);
-		}
-
-		// POST: Support/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> DeleteConfirmed(int id)
-		{
 			var support = await _context.Supports.FindAsync(id);
 			_context.Supports.Remove(support);
 			await _context.SaveChangesAsync();
 			return RedirectToAction(nameof(Index));
 		}
 
-		private bool SupportExists(int id)
-		{
-			return _context.Supports.Any(e => e.ID == id);
-		}
+
 	}
 }
--- ProjectSupportSystem/Models/SupportElement.cs
+++ ProjectSupportSystem/Models/SupportElement.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,15 @@ namespace ProjectSupportSystem.Models
 {
 	public class SupportElement : Base
 	{
+		[DisplayName("Adı")]
 		public string SupportElementName { get; set; }
+
+		[DisplayName("Oranı (%)")]
 		public int SupportElementPercentage { get; set; }
+
+		[DisplayName("Miktarı (TL)")]
 		public decimal SupportElementAmount { get; set; }
+
 		public int SupportSupElementID { get; set; }
 		public virtual List<SupportSupElement> SupportsOfSupportSupElement { get; set; }
 	}
