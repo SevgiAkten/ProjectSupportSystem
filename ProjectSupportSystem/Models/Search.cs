ea85f7112f43b434ab316bd6c194f9d953565132
Sevgi AKTEN <sevgiakten@gmail.com>
2020-04-10 00:01:38 +0300

    LISTING ALL SUPPORTS AND SEARCH VİEW WAS CODING

---
+++ ProjectSupportSystem/Models/Search.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSupportSystem.Models
+{
+	public class Search
+	{
+		public bool Kosgeb { get; set; }
+		public bool Tubitak { get; set; }
+		public bool TicBak { get; set; }
+		public decimal minAmount { get; set; }
+		public decimal maxAmount { get; set; }
+		public decimal minPercentage { get; set; }
+		public decimal maxPercentage { get; set; }
+		public decimal minPDuration { get; set; }
+		public decimal maxDuration { get; set; }
+	}
+}
---
+++ ProjectSupportSystem/Models/SupportViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSupportSystem.Models
+{
+	public class SupportViewModel
+	{
+		public List<Support> Support { get; set; }
+		public Search Search { get; set; } 
+	}
+}
